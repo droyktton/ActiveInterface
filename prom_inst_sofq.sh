57eb74563499f5f7e81bbaadc2e027cf3c20e71c
AleK <droyktton@gmail.com>
2025-10-23 10:23:37 -0300

    program update

---
+++ prom_inst_sofq.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+dir=$1
+
+samples=$(ls $dir/inst_sofq.dat | wc -l)
+
+paste $dir/inst_sofq.dat | awk '{acum1=0.0; acum2=0.0; n=0;for(i=1;i<=NF;i+=2){acum1+=$i;acum2+=$(i+1);n++;}; if(NF>0) print acum1*1.0/n, acum2*1.0/n; else print;}' \
+> "sofq_"$samples"samples.dat"
+
+file="sofq_"$samples"samples.dat"
+echo $file
+
+gnuplot -p -e "set term png; set out 'sofq.png';set logs; plot for[i=0:7] '$file' index i u 0:1 w lp t ''"
